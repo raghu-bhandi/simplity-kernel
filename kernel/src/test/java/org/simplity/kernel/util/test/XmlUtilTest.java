b6d258f45b2721ed6d60191cc8b08e5016057330
Praveen_Buya <Praveen_Buya@HYDHTC242098D.ad.infosys.com>
2016-08-18 11:26:25 +0530

    Changed return type of XmlUtil - xmlToObject method,added junit and test case for xmltoobject creation

---
+++ kernel/src/test/java/org/simplity/kernel/util/test/XmlUtilTest.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package org.simplity.kernel.util.test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.simplity.kernel.util.XmlUtil;
+import org.simplity.tp.Service;
+
+public class XmlUtilTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		 
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.simplity.kernel.util.XmlUtil#xmlToObject(java.io.InputStream, java.lang.Object)}.
+	 */
+	@Test
+	public final void testXmlToObject() {
+		Object object = new Service();
+		Object object1 = null;
+		try {
+			File file = new File("D:\\Test\\SimplityXML\\NewFile.xml");
+			FileInputStream fis = null;
+			
+			fis = new FileInputStream(file);
+
+			
+			 object1= XmlUtil.xmlToObject(fis, object);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertNotNull(object1);
+		assertEquals(Service.class, object1.getClass());
+		
+	}
+
+}
