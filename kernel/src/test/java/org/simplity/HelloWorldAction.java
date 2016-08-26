dbd9368a29f0929aabfd97bae6cb177d230611ba
Praveen_Buya <Praveen_Buya@HYDHTC242098D.ad.infosys.com>
2016-08-26 18:54:41 +0530

    check details of outputrecords

--- kernel/src/test/java/org/simplity/HelloWorldAction.java
+++ kernel/src/test/java/org/simplity/HelloWorldAction.java
@@ -14,8 +14,19 @@ public class HelloWorldAction implements LogicInterface {
 
 	@Override
 	public Value execute(ServiceContext ctx) {
-		Value value = Value.newTextValue("Hellooooo World from a Java class 1234sdfas !!");
+		Value value = Value.newTextValue("11111");
 		ctx.setValue("hello", value);
+		
+		Value value1 = Value.newTextValue("222222");
+		ctx.setValue("hello1", value1);
+		
+		Value value2 = Value.newTextValue("33333");
+		ctx.setValue("hello2", value2);
+		
+		Value value3 = Value.newTextValue("444444");
+		ctx.setValue("hello1", value3);
+		
+		
 		return value;
 
 	}
