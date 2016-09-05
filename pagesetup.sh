e00bd641cf0ca6017ff18f4713840a1e4a2c9d5c
Chetana <chetana_shanbhag@infosys.com>
2016-09-05 17:48:12 +0530

    modified pagesetup

--- pagesetup.sh
+++ pagesetup.sh
@@ -5,8 +5,6 @@ git clone -b gh-pages https://github.com/simplity/simplity.git
 cd ..
 cp -r target/site/apidocs javadocs/simplity 
 cd javadocs/simplity
-git config --global user.email "archetana@gmail.com"
-git config --global user.name "Chetana"
 git add .
 git commit -m "gh pages javadocs"
 git push origin gh-pages
