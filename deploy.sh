c212da7ba03decf5bb3a7b6912b5b05b0a994c72
Chetana <chetana_shanbhag@infosys.com>
2016-09-06 10:24:30 +0530

    travis gh-pages 1

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  ./pagesetup.sh
+  ./compile.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -50,15 +50,15 @@ fi
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+# Get the deploy key by using Travis's stored variables to decrypt sshtravis.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -javadocs deploy_key -d
-chmod 600 deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in sshtravis.enc -javadocs sshtravis -d
+chmod 600 sshtravis
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add sshtravis
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
