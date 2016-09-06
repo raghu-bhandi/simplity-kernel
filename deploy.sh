d26c890b425047879e75a3089e19a5e1b93e3ca1
Chetana <chetana_shanbhag@infosys.com>
2016-09-06 11:31:43 +0530

    travis gh-pages 3

--- deploy.sh
+++ deploy.sh
@@ -56,7 +56,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in sshtravis.enc -javadocs sshtravis -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in sshtravis.enc -out sshtravis -d
 chmod 600 sshtravis
 eval `ssh-agent -s`
 ssh-add sshtravis
