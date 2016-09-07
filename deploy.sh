5f26e399d9775a2802d39f8ab038ea178898233e
Chetana <chetana_shanbhag@infosys.com>
2016-09-07 15:39:32 +0530

    test ssh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,13 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+echo "Hello 1"
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
+echo "Hello 2"
 eval `ssh-agent -s`
+
+echo "Hello 3"
 ssh-add deploy_key
 
 SOURCE_BRANCH="master"
