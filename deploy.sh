355029ab42af3dea387108ac0a4eea3b8d00cf24
Chetana <chetana_shanbhag@infosys.com>
2016-09-07 15:24:50 +0530

    adding ssh support

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
-
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
 # Get the deploy key by using Travis's stored variables to decrypt sshtravis.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
@@ -18,6 +10,17 @@ openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out 
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+
 
 # Now that we're all set up, we can push.
 #git push $SSH_REPO $TARGET_BRANCH
