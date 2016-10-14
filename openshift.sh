fc804ba017b1913cd9cd3585582c66f241827b32
Chetana Parameshwar Shanbhag <chetana_shanbhag@infosys.com>
2016-10-14 13:50:54 +0530

    Retaining the core files only

--- openshift.sh
+++
@@ -1,8 +0,0 @@
-cd app-root/repo/
-mvn install
-echo "Completed mvn install"
-cd ~
-rm -r jbossews/webapps/*
-cp app-root/repo/example/target/example.war jbossews/webapps/
-cd jbossews/webapps/
-mv example.war ROOT.war
