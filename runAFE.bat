19b1dbe643b0d775dc11c8d790f14054e38c183b
Chetana Parameshwar Shanbhag <chetana_shanbhag@infosys.com>
2016-10-21 14:27:01 +0530

    modified gitlab-ci

--- runAFE.bat
+++ runAFE.bat
@@ -1,10 +1,5 @@
 if not exist "D:\Projects\simplity" mkdir "D:\Projects\simplity"
 cd /D D:\Projects\simplity
-git init
-git remote add -f origin http://infygit.ad.infosys.com/encore/Simplity.git
-git config core.sparseCheckout true
-echo example/*  1>>.git/info/sparse-checkout
-echo AFE2016/*  1>>.git/info/sparse-checkout
 git pull origin master
 cd AFE2016
 call mvn clean install
