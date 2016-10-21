0ff2229f4bf79e867ed2c818f259a48cdd301080
Chetana Parameshwar Shanbhag <chetana_shanbhag@infosys.com>
2016-10-21 12:29:16 +0530

    Fixed deploy path issues

--- runAFE.bat
+++ runAFE.bat
@@ -3,7 +3,10 @@ cd /D D:\Projects\simplity
 git init
 git remote add -f origin http://infygit.ad.infosys.com/encore/Simplity.git
 git config core.sparseCheckout true
+echo example/*  1>>.git/info/sparse-checkout
 echo AFE2016/*  1>>.git/info/sparse-checkout
 git pull origin master
 cd AFE2016
+call mvn clean install
+cd ..\example
 call mvn clean install
