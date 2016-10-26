179ffe0abf60c281b877e314876247be6c27f54f
Chetana Parameshwar Shanbhag <chetana_shanbhag@infosys.com>
2016-10-26 17:48:38 +0530

    Revert "Junit to StrutsApp"
    This reverts commit 8ed3c615b9456ab5980e3af662aca217e03d5678.

--- StrutsApp/src/test/java/com/infosys/example/HelloWorldTest.java
+++ StrutsApp/src/test/java/com/infosys/example/HelloWorldTest.java
@@ -22,85 +22,15 @@
 package com.infosys.example;
 
 import com.opensymphony.xwork2.ActionSupport;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.servlet.ServletContext;
-
 import org.apache.struts2.StrutsTestCase;
-import org.junit.BeforeClass;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.simplity.kernel.Application;
-import org.simplity.kernel.file.FileManager;
-import org.simplity.kernel.util.XmlParseException;
-import org.simplity.kernel.util.XmlUtil;
 
 public class HelloWorldTest extends StrutsTestCase {
 
-	final static String COMP_PATH = "src/main/webapp/WEB-INF/";	
-	static Mockito mockito = new Mockito();
-	
-	public static void setup(){
-		ServletContext context = Mockito.mock(ServletContext.class);
-		FileManager.setContext(context);
-		
-		Mockito.when(context.getResourceAsStream(Mockito.anyString())).then(new Answer<InputStream>() {
-			public InputStream answer(InvocationOnMock invocation) throws Throwable {
-				InputStream is = null;
-				String newPath = (String) invocation.getArguments()[0];
-				if(newPath.startsWith("/")){
-					newPath = newPath.substring(1);
-				}
-				File file = new File(newPath);
-				if(file.exists()){
-					is = new FileInputStream(file);
-					return is;
-				}
-				
-				file = new File(COMP_PATH,newPath);
-				if(file.exists()){
-					is = new FileInputStream(file);
-					return is;
-				}
-				return null;
-			}			
-		});
-		
-		Mockito.when(context.getResourcePaths(Mockito.anyString())).then(new Answer<Set<String>>() {
-
-			public Set<String> answer(InvocationOnMock invocation) throws Throwable {
-				File folder = new File(COMP_PATH,(String)invocation.getArguments()[0]);
-				Set<String> filePaths = new HashSet<String>();
-				for (File fileItem : folder.listFiles()) {
-					filePaths.add(fileItem.getAbsolutePath());
-				}
-				return filePaths;
-			}
-		});
-		
-		Application app = new Application();
-		try {
-			XmlUtil.xmlToObject(COMP_PATH + "comp/application.xml", app);
-		} catch (XmlParseException e) {
-			e.printStackTrace();
-		}
-		app.configure();
-	}
-
     public void testHelloWorld() throws Exception {
-    	HelloWorldTest.setup();
-    	
         HelloWorld hello_world = new HelloWorld();
         String result = hello_world.execute();
         assertTrue("Expected a success result!",
                 ActionSupport.SUCCESS.equals(result));
-        System.out.println(hello_world.getMessage());
         assertTrue("Expected the default message!",
                 hello_world.getText(HelloWorld.MESSAGE).equals(hello_world.getMessage()));
     }
