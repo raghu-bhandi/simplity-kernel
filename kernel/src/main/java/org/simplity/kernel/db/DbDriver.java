1ddfa9e0cfd9bad909844d1089fa01a51289f226
RG Bhandi <raghu.bhandi@gmail.com>
2016-11-03 15:19:13 +0530

    Issue with oracle driver fixed with upfront unwrap() for pooled connection

--- kernel/src/main/java/org/simplity/kernel/db/DbDriver.java
+++ kernel/src/main/java/org/simplity/kernel/db/DbDriver.java
@@ -41,6 +41,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import oracle.jdbc.driver.OracleConnection;
 import oracle.sql.ARRAY;
 import oracle.sql.ArrayDescriptor;
 import oracle.sql.STRUCT;
@@ -78,11 +79,11 @@ public class DbDriver {
 	 * we store sql types with corresponding value types
 	 */
 	private static final int[] LONG_TYPES = { Types.BIGINT, Types.INTEGER,
-		Types.SMALLINT };
+			Types.SMALLINT };
 	private static final int[] DATE_TYPES = { Types.DATE, Types.TIME,
-		Types.TIMESTAMP };
+			Types.TIMESTAMP };
 	private static final int[] DOUBLE_TYPES = { Types.DECIMAL, Types.DOUBLE,
-		Types.FLOAT, Types.REAL };
+			Types.FLOAT, Types.REAL };
 	private static final int[] BOOLEAN_TYPES = { Types.BIT, Types.BOOLEAN };
 	private static final Map<Integer, ValueType> SQL_TYPES = new HashMap<Integer, ValueType>();
 
@@ -108,7 +109,7 @@ public class DbDriver {
 	 */
 	private static final int TABLE_IDX = 0;
 	private static final String[] TABLE_NAMES = { "schema", "tableName",
-		"tableType", "remarks" };
+			"tableType", "remarks" };
 	private static final ValueType[] TABLE_TYPES = { TXT, TXT, TXT, TXT };
 	private static final int[] TABLE_POSNS = { 2, 3, 4, 5 };
 	private static final String[] TABLE_TYPES_TO_EXTRACT = { "TABLE", "VIEW" };
@@ -117,10 +118,10 @@ public class DbDriver {
 	 */
 	private static final int COL_IDX = 1;
 	private static final String[] COL_NAMES = { "schema", "tableName",
-		"columnName", "sqlType", "sqlTypeName", "size", "nbrDecimals",
-		"remarks", "nullable", "autoIncrement" };
+			"columnName", "sqlType", "sqlTypeName", "size", "nbrDecimals",
+			"remarks", "nullable", "autoIncrement" };
 	private static final ValueType[] COL_TYPES = { TXT, TXT, TXT, INT, TXT,
-		INT, INT, TXT, BOOL, BOOL };
+			INT, INT, TXT, BOOL, BOOL };
 	private static final int[] COL_POSNS = { 2, 3, 4, 5, 6, 7, 9, 12, 18, 23 };
 
 	/*
@@ -136,7 +137,7 @@ public class DbDriver {
 	 */
 	private static final int PROC_IDX = 3;
 	private static final String[] PROC_NAMES = { "schema", "procedureName",
-		"procedureType", "remarks" };
+			"procedureType", "remarks" };
 	private static final ValueType[] PROC_TYPES = { TXT, TXT, INT, TXT };
 	private static final int[] PROC_POSNS = { 2, 3, 8, 7 };
 
@@ -145,19 +146,19 @@ public class DbDriver {
 	 */
 	private static final int PARAM_IDX = 4;
 	private static final String[] PARAM_NAMES = { "schema", "procedureName",
-		"paramName", "columnType", "sqlType", "sqlTypeName", "size",
-		"precision", "scale", "remarks", "nullable", "position" };
+			"paramName", "columnType", "sqlType", "sqlTypeName", "size",
+			"precision", "scale", "remarks", "nullable", "position" };
 	private static final ValueType[] PARAM_TYPES = { TXT, TXT, TXT, INT, INT,
-		TXT, INT, INT, INT, TXT, BOOL, INT };
+			TXT, INT, INT, INT, TXT, BOOL, INT };
 	private static final int[] PARAM_POSNS = { 2, 3, 4, 5, 6, 7, 9, 8, 10, 13,
-		19, 18 };
+			19, 18 };
 
 	/*
 	 * names, types and positions as per result set for meta.getUDTs()
 	 */
 	private static final int STRUCT_IDX = 5;
 	private static final String[] STRUCT_NAMES = { "schema", "structName",
-		"structType", "remarks" };
+			"structType", "remarks" };
 	private static final ValueType[] STRUCT_TYPES = { TXT, TXT, TXT, TXT };
 	private static final int[] STRUCT_POSNS = { 2, 3, 5, 6 };
 	private static final int[] STRUCT_TYPES_TO_EXTRACT = { Types.STRUCT };
@@ -166,21 +167,21 @@ public class DbDriver {
 	 */
 	private static final int ATTR_IDX = 6;
 	private static final String[] ATTR_NAMES = { "schema", "structName",
-		"attributeName", "sqlType", "sqlTypeName", "size", "nbrDecimals",
-		"remarks", "nullable", "position" };
+			"attributeName", "sqlType", "sqlTypeName", "size", "nbrDecimals",
+			"remarks", "nullable", "position" };
 	private static final ValueType[] ATTR_TYPES = { TXT, TXT, TXT, INT, TXT,
-		INT, INT, TXT, BOOL, INT };
+			INT, INT, TXT, BOOL, INT };
 	private static final int[] ATTR_POSNS = { 2, 3, 4, 5, 6, 7, 8, 11, 17, 16 };
 
 	/*
 	 * put them into array for modularity
 	 */
 	private static final String[][] META_COLUMNS = { TABLE_NAMES, COL_NAMES,
-		KEY_NAMES, PROC_NAMES, PARAM_NAMES, STRUCT_NAMES, ATTR_NAMES };
+			KEY_NAMES, PROC_NAMES, PARAM_NAMES, STRUCT_NAMES, ATTR_NAMES };
 	private static final ValueType[][] META_TYPES = { TABLE_TYPES, COL_TYPES,
-		KEY_TYPES, PROC_TYPES, PARAM_TYPES, STRUCT_TYPES, ATTR_TYPES };
+			KEY_TYPES, PROC_TYPES, PARAM_TYPES, STRUCT_TYPES, ATTR_TYPES };
 	private static final int[][] META_POSNS = { TABLE_POSNS, COL_POSNS,
-		KEY_POSNS, PROC_POSNS, PARAM_POSNS, STRUCT_POSNS, ATTR_POSNS };
+			KEY_POSNS, PROC_POSNS, PARAM_POSNS, STRUCT_POSNS, ATTR_POSNS };
 
 	static {
 		for (int i : LONG_TYPES) {
@@ -331,7 +332,7 @@ public class DbDriver {
 	private static void setDataSource(String schema, String dataSourceName) {
 		try {
 			DataSource ds = (DataSource) new InitialContext()
-			.lookup(dataSourceName);
+					.lookup(dataSourceName);
 			Connection con = ds.getConnection();
 			if (schema == null) {
 				defaultSchema = extractDefaultSchema(con);
@@ -1262,7 +1263,7 @@ public class DbDriver {
 			sbf.append('\n').append(++i).append(" : ").append(value.toString());
 			if (i > 12) {
 				sbf.append("..like wise up to ").append(values.length)
-				.append(" : ").append(values[values.length - 1]);
+						.append(" : ").append(values[values.length - 1]);
 				break;
 			}
 		}
@@ -1617,9 +1618,10 @@ public class DbDriver {
 			}
 		}
 		if (dbVendor == DbVendor.ORACLE) {
+			OracleConnection ocon = con.unwrap(OracleConnection.class);
 			ArrayDescriptor ad = ArrayDescriptor.createDescriptor(dbArrayType,
-					con);
-			return new ARRAY(ad, con, data);
+					ocon);
+			return new ARRAY(ad, ocon, data);
 		}
 		return con.createArrayOf(dbArrayType, data);
 	}
@@ -1639,9 +1641,10 @@ public class DbDriver {
 	public static Struct createStruct(Connection con, Object[] data,
 			String dbObjectType) throws SQLException {
 		if (dbVendor == DbVendor.ORACLE) {
+			OracleConnection ocon = con.unwrap(OracleConnection.class);
 			StructDescriptor sd = StructDescriptor.createDescriptor(
-					dbObjectType, con);
-			return new STRUCT(sd, con, data);
+					dbObjectType, ocon);
+			return new STRUCT(sd, ocon, data);
 		}
 		return con.createStruct(dbObjectType, data);
 	}
@@ -1685,9 +1688,10 @@ public class DbDriver {
 	public static Array createStructArray(Connection con, Struct[] structs,
 			String dbArrayType) throws SQLException {
 		if (dbVendor == DbVendor.ORACLE) {
+			OracleConnection ocon = con.unwrap(OracleConnection.class);
 			ArrayDescriptor ad = ArrayDescriptor.createDescriptor(dbArrayType,
-					con);
-			return new ARRAY(ad, con, structs);
+					ocon);
+			return new ARRAY(ad, ocon, structs);
 		}
 		return con.createArrayOf(dbArrayType, structs);
 	}
