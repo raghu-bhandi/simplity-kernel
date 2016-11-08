222ffeaf4b05e376002d95001cf65b5f13f1e3a5
Shalinireddy_B <Shalinireddy_B@BLRKEC382208D.ad.infosys.com>
2016-11-08 19:02:23 +0530

    Validations and data lookup added

--- AFE2016/src/main/java/com/infosys/submission/util/Mail.java
+++ AFE2016/src/main/java/com/infosys/submission/util/Mail.java
@@ -1,6 +1,5 @@
 package com.infosys.submission.util;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -8,14 +7,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.simplity.kernel.value.Value;
-import org.simplity.service.ServiceAgent;
 import org.simplity.service.ServiceContext;
 import org.simplity.tp.LogicInterface;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
-import freemarker.template.TemplateExceptionHandler;
 
 public class Mail  implements LogicInterface{
 
@@ -25,10 +22,19 @@ public class Mail  implements LogicInterface{
 		parameters.put ("submitter", ctx.getTextValue("submitterId"));
 		parameters.put ("sponsor", ctx.getTextValue("sponsormailid"));
 		parameters.put ("title", ctx.getTextValue("nomination"));
+		parameters.put ("status", ctx.getTextValue("status"));
 		
 		Writer textValue = new OutputStreamWriter(System.out);
+		Template template = null;
 		try {
-			Template template = cfg.getTemplate ("submit.ftlh");
+			switch(parameters.get("status").toString()){
+			case "Submitted":
+				template = cfg.getTemplate ("submit.ftlh");
+			case "Approved":
+				template = cfg.getTemplate ("approve.ftlh");
+			case "Rejected":
+				template = cfg.getTemplate ("reject.ftlh");
+			}
 			template.process (parameters,textValue );
 		} catch (IOException e) {
 			e.printStackTrace();
