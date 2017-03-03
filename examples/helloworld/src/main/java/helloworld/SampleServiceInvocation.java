63e9e712b895c739bdf0d0fd1a340bf68a7907d8
Chetana Amancharla <Chetana_Shanbhag@ad.infosys.com>
2017-03-03 13:03:19 +0530

    helloworld example

--- examples/helloworld/src/main/java/helloworld/SampleServiceInvocation.java
+++ examples/helloworld/src/main/java/helloworld/SampleServiceInvocation.java
@@ -1,5 +1,29 @@
 package helloworld;
 
-public class SampleServiceInvocation {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.simplity.service.JavaAgent;
+
+public class SampleServiceInvocation extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		JavaAgent ja = new JavaAgent().getAgent("100", null);
+		String outdata = ja.serve("fileprocessing", null);
+		System.out.println(outdata);
+		PrintWriter writer = resp.getWriter();
+		writer.write(outdata);
+		writer.flush();
+		writer.close();
+		
+	}
 
 }
