724b56d18d4cd4abe5bc4128298a7efd9167961c
Shalini Reddy Bommana <Shalinireddy_B@ad.infosys.com>
2017-03-10 12:12:49 +0530

    helloworld example modified

---
+++ examples/helloworld/src/main/java/helloworld/ProcessErrorRows.java
@@ -0,0 +1,24 @@
+package helloworld;
+
+import java.util.List;
+
+import org.simplity.kernel.FormattedMessage;
+import org.simplity.kernel.value.Value;
+import org.simplity.service.ServiceContext;
+import org.simplity.tp.LogicInterface;
+
+public class ProcessErrorRows implements LogicInterface {
+
+	@Override
+	public Value execute(ServiceContext ctx) {
+		System.out.println("Hello");
+		List<FormattedMessage> msgs = ctx.getMessages();
+		for(FormattedMessage msg:msgs){
+			System.out.println(msg.text);
+			System.out.println(msg.data[0]);
+			System.out.println(msg.fieldName);
+		}
+		return null;
+	}
+
+}
