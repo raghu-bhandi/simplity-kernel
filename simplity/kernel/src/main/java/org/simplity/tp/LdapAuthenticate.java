29838e199a39dff67ab62fdc98fab37c64e25623
Chetana Amancharla <Chetana_Shanbhag@ad.infosys.com>
2017-04-07 18:32:15 +0530

    added LdapAuthenticate LdapLookup

---
+++ simplity/kernel/src/main/java/org/simplity/tp/LdapAuthenticate.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2017 simplity.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.simplity.tp;
+
+import java.util.Hashtable;
+
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+
+import org.simplity.kernel.ldap.LdapAgent;
+import org.simplity.kernel.value.Value;
+import org.simplity.service.ServiceContext;
+import org.simplity.service.ServiceProtocol;
+
+/**
+ * @author simplity.org
+ *
+ */
+public class LdapAuthenticate extends Action {
+	private DirContext ldapCtx;
+	
+	String principal;
+	String credentials;
+
+	@Override
+	protected Value doAct(ServiceContext ctx) {
+		Hashtable<String, String> env = new Hashtable<String, String>(11);
+		try {
+			// Create initial context
+			DirContext ldapCtx = LdapAgent.getInitialDirContext(principal,credentials);
+			if(ldapCtx == null){
+				return Value.VALUE_FALSE;
+			}
+			// Close the context when we're done
+			ldapCtx.close();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		return Value.VALUE_TRUE;
+	}
+}
