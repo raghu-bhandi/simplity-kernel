9bce65ab43bcd70152b490a79e2ae9e740ee7985
Chetana Amancharla <Chetana_Shanbhag@ad.infosys.com>
2017-04-12 19:34:55 +0530

    SendMail

---
+++ simplity/kernel/src/main/java/org/simplity/tp/Content.java
@@ -0,0 +1,12 @@
+package org.simplity.tp;
+
+public class Content {
+	ContentType type;
+	String templatePath;
+	String template;
+	String[] inputSheetName;
+	String text;
+
+	public Content() {
+	}
+}
